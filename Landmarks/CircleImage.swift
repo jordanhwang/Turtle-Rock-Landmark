8b6df728bae2214bd4b3ea6bd074e80fc7fc268b
jordanhwang <jordanwhwang@gmail.com>
2025-03-03 07:44:12 -0500

    Turtle Rock App
    Provides a description of Turtle Rock and uses MapKit to show the location.

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by JORD on 3/3/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Turtle Rock in Joshua Tree National Park is a large, rounded rock formation resembling a turtle’s shell. It's a popular spot for bouldering, hiking, and photography, offering scenic views of the desert landscape. Located near the Hidden Valley area, it's easily accessible and a great example of the park’s unique granite formations.")
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
